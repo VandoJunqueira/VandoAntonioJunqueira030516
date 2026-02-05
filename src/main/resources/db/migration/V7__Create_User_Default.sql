db868ae68d113a9f0e5947cc77f3f0149284f22d
Vando Junqueira <vandojunqueira@gmail.com>
2026-02-04 21:14:23 -0400

    fix: Criptografa senha do usuário padrão na migração do banco
    - Atualiza o script V7__Create_User_Default.sql substituindo a senha em texto plano pelo hash BCrypt para maior segurança.
    - Adiciona PasswordGeneratorTest para auxiliar na geração de hashes de senha para desenvolvimento.

--- src/main/resources/db/migration/V7__Create_User_Default.sql
+++ src/main/resources/db/migration/V7__Create_User_Default.sql
@@ -1,4 +1,8 @@
 INSERT INTO
     users (username, password, role)
 VALUES
-    ('admin', 'password', 'ADMIN');
+    (
+        'admin',
+        '$2a$10$Ha7gNkzSZ3B/Fvvwa.VllOIhkN10UwSq6K02x9nL9qexA09KviTwq',
+        'ADMIN'
+    );
